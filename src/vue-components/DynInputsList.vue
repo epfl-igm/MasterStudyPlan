57f86168495b9ae9258ffb81e691e02ab433e494
Loïc Humbert <loic.humbert@epfl.ch>
2018-06-04 13:20:50 +0200

    Vue component for dynamic inputs list

---
+++ src/vue-components/DynInputsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="(inputValue, index) in values">
+      <input type="text" v-model="values[index]">
+      <!--input type="text" v-bind:value="value" v-bind:name="name + '[]'"-->
+      <a href='javascript:void(0);' @click="add">Add</a>
+      <a href='javascript:void(0);' @click="remove(index)">Remove</a>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: "dyn-inputs-list",
+  props: {
+    csv: Array,
+    name: String,
+  },
+  data: function() {
+    return {
+      values: this.csv || [''],
+    }
+  },
+  methods: {
+    add: function () {
+      this.values.push( '' )
+    },
+    remove: function (index) {
+      this.values.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
